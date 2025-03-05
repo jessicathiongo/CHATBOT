c3148f8c89778772fa8fcb44cdb391037c7e4898
jessicathiongo <jessicajthiongo@gmail.com>
2025-03-05 13:00:30 +0300

    updated link

--- chat bot.js
+++ chat bot.js
@@ -25,7 +25,7 @@ async function handleUserMessage() {
   showTypingIndicator();
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/chat", {
+    const response = await fetch("https://thera-ezac.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text: message }),
